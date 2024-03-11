955ee475fef183471ed4b06559a1469b7f19d76b
MrVagabond <43123504+MrVagabond@users.noreply.github.com>
2024-03-12 02:34:33 +0800

    Update bucketsort.py

--- bucketsort.py
+++ bucketsort.py
@@ -1,11 +1,14 @@
 @check
 def bucketsort(arr:List[int], k:int) -> List[int]:
+    from pygt2.mychecker import TypeCheckMemo
+    from pygt2.mychecker._functions import check_argument_types, check_return_type
+    memo = TypeCheckMemo(globals(), locals())
+    check_argument_types('bucketsort', {'arr': (arr, List[int]), 'k': (k,
+        int)}, memo)
     counts = [0] * k
     for x in arr:
         counts[x] += 1
-
     sorted_arr = []
     for i, count in enumerate(counts):
         sorted_arr.extend([i] * count)
-
-    return sorted_arr
+    return check_return_type('bucketsort', sorted_arr, List[int], memo)
